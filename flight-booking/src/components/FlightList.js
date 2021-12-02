86b8fe640c9775b838160498957f75bd3b8df72c
payal-saha <payalsaha.kv@gmail.com>
2021-12-02 12:27:41 +0530

    Commit for 2nd ui

--- flight-booking/src/components/FlightList.js
+++ flight-booking/src/components/FlightList.js
@@ -4,6 +4,7 @@ import  '../styles/flightlist.css';
  function FlightList() {
     return (
         <>
+        Hello
         <div className="container">
             <div className="detail_box">Detail</div>
             <div className="list_box">List</div>
