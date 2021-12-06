7dee5cef69598c4e52cc7f15a1bfe71817b206b3
Navaneeth Asok <A-10110@ibsplc.com>
2021-12-06 15:21:10 +0530

    flight details in progress

--- flight-booking/src/components/FlightDetails.js
+++ flight-booking/src/components/FlightDetails.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/container.css'
 
 function FlightDetails() {
     return (
@@ -7,11 +8,22 @@ function FlightDetails() {
                 <h2>Airline Name/code</h2>
  
                 <div>
-                    <h2>Date</h2>
+                    <h2 className="date">Thursday, Nov 25</h2>
                 </div>
-                <div>
-                    <h3>Class</h3>
+                <div className="cabin">
+                    <h3>Class:</h3>
                     <h2>Premium Economy</h2>
+                </div>
+            </div>
+
+            <div className="flight-details">
+                <div className="airport-row">
+                    <h3>07:55</h3>
+                    <span className="dot"></span>
+                    <div className="airport">
+                        <h3>New Delhi</h3>
+                        <p>Indira Gandhi International Airport <br/> Terminal 1</p>
+                    </div>
                 </div>
             </div>
         </div>
