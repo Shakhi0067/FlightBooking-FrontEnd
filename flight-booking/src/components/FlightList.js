fb39ca1adaafabb11a031cba9fd4214853f4c245
navaneethasok <asok.navaneeth@gmail.com>
2021-12-08 12:41:20 +0530

    api integration in progress

---
+++ flight-booking/src/components/FlightList.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+=======
+import React from 'react'
+import  '../styles/flightlist.css';
+
+ function FlightList() {
+    return (
+        <>
+        Hello
+        <div className="container">
+            <div className="detail_box">Detail</div>
+            <div className="list_box">List</div>
+        </div>
+        </>
+    )
+}
+export default  FlightList;
+>>>>>>> 86b8fe6 (Commit for 2nd ui)
