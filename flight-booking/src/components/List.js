220da0351448df0e771b9b0cb6d18d25e54e7116
payal-saha <payalsaha.kv@gmail.com>
2021-12-08 12:34:58 +0530

    changes made in 2nd ui

--- flight-booking/src/components/List.js
+++ flight-booking/src/components/List.js
@@ -1,72 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { Table } from 'react-bootstrap'
 import  '../styles/list.css';
 function List() {
+    const [flights, setflights] = useState([])
+
+    const refreshList=()=>{
+        const url = 'http://jsonplaceholder.typicode.com/users'
+        axios.get(url).then((response) => {
+            setflights(response.data)
+        })
+    }
+
+
+   useEffect(()=>{
+       refreshList()
+   },[])
     return (
         <div>
-            <center><h1>Best Flights </h1><br/></center>
-            <hr width="800px;" size="2" />
+            
+            <center><h1 className='Flight_Heading'>Best Flights </h1></center>
+            <hr width="900px;" size="2" className='border'/>
             <Table>
            
             <tbody>
-                    {
-                       
-                                <tr className="Clickable">
-                                
-                                <td>flightName</td>
-                                <td> 
-                                    departureHour:departureMinute<br/>
-                                    sourceAirport</td>
-                                <td>duration<hr/></td>
+            {
+                        flights.filter((flight, idx) => idx < 3).map((flight)=>{
+                            return(
+                                <>
+                                <tr key={flight.id} className='Clickable'>
+                                <td><b>{flight.id}</b></td>
+                                <td>{flight.name}</td>
+                                <td>{flight.username}</td>
+                                <td>{flight.email}<hr className='duration'/></td>
                                 <td>
-                                    arrivalHour:arrivalMinute<br/>
-                                    destinationAirport<br/></td>
-                                <td>price</td>
-                                
-                               </tr>
-                            
-                       
-                    }<br/>
-                    {
-                       
-                            <tr className="Clickable">
-                       
-                            <td>flightName</td>
-                            <td> 
-                           departureHour:departureMinute<br/>
-                           sourceAirport</td>
-                            <td>duration<hr/></td>
-                            <td>
-                           arrivalHour:arrivalMinute<br/>
-                           destinationAirport<br/></td>
-                            <td>price</td>
-                      
-                            </tr>
-                   
-              
-                     }<br/><br/>
-                    {
-                       
-                       <tr className="Clickable">
-                       
-                       <td>flightName</td>
-                       <td> 
-                           departureHour:departureMinute<br/>
-                           sourceAirport</td>
-                       <td>duration<hr/></td>
-                       <td>
-                           arrivalHour:arrivalMinute<br/>
-                           destinationAirport<br/></td>
-                       <td>price</td>
-                      
-                      </tr>
-                   
-              
+                                    {flight.address.street}<br/>
+                                    {flight.address.city}:{flight.suite}
+                                </td>
+                                <td>
+                                {flight.company.name}<br/>
+                                    {flight.company.bs}:{flight.catchPhrase}</td>
+                                <td>{flight.phone}</td>
+                                </tr>
+                                <br/>
+                                </>
+                            )
+                        })
                     }
                      
                 </tbody>
             </Table>
         </div>
-    )
-}
+        )
+    }
 export default  List;
