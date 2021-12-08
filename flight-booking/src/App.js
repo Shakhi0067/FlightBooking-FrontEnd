8b6cfef88c0f096c0c3e300310995d070c5de0cb
navaneethasok <asok.navaneeth@gmail.com>
2021-12-08 13:09:52 +0530

    api integration in progress

--- flight-booking/src/App.js
+++ flight-booking/src/App.js
@@ -1,28 +1,12 @@
 import './App.css';
-<<<<<<< HEAD
 import SelectedFlight from './components/SelectedFlight';
-=======
-import SearchLocations from './components/SearchLocations';
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react'
-import { Switch, Route } from 'react-router-dom';
->>>>>>> main
 
 function App() {
   return (
 
-    
     <>
-<<<<<<< HEAD
       <SelectedFlight/>
     </>
-=======
-     
-     <SearchLocations/>
-       
-       </>
-
->>>>>>> main
 
 
     
