8e32c1566c53330151c07eec950f6224b77050c7
Nimishasreejaa <A-10172@ibsplc.com>
2021-12-10 16:58:06 +0530

    api integration

--- flight-booking/src/App.js
+++ flight-booking/src/App.js
@@ -1,17 +1,32 @@
-import './App.css';
-import SelectedFlight from './components/SelectedFlight';
+import React, { Component } from 'react';
+import FlightList from './components/FlightList';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    data: {},
+    price: {}
+  }
 
-    <>
-      <SelectedFlight/>
-    </>
+  componentDidMount() {
+    fetch('http://localhost:8080/api/flights?origin=COK&destination=LON&departDate=2022-01-25&adults=3&travelClass=ECONOMY&returnDate=2022-01-26')
+      .then(res => res.json()).then((data) => {
+        const iteneries = data[0].itineraries.reduce(function (acc, itr) {
+          acc[itr.duration] = itr.segments
+          return acc
+        }, {})
+        this.setState({ data: iteneries, price: data[0].price })
+      })
+      .catch(console.log)
+  }
 
 
-    
 
-  );
+  render() {
+    return (
+      <FlightList flights={this.state} />
+    )
+  }
 }
 
 export default App;
+
--- flight-booking/src/components/FlightList.js
+++ flight-booking/src/components/FlightList.js
@@ -1,17 +1,30 @@
 import React from 'react'
-import  '../styles/flightlist.css';
-import Detail from './Detail';
-import List from './List';
- function FlightList() {
-    return (
-        <>
-        
-        <div className="container">
-            
-            <div className="detail_box"><Detail/></div>
-            <div className="list_box"><List/></div>
-        </div>
-        </>
-    )
+
+const airCraft = {
+    "32B": "Airbus 32 B",
+    "788": "Airbus 788"
 }
-export default  FlightList;
+
+const FlightList = ({ flights }) => {
+    const value = Object.keys(flights.data)
+    return (
+        <div>
+            <center><h1>Flight List</h1></center>
+            {value?.map((duration) => ( // PT14, PT15
+                flights.data[duration].map((items, index) => (
+                    <div key={index} className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Flight Code: {airCraft[items.aircraft.code]}</h5>
+                            <h5 className="card-text">Travel Duration: {items.duration.split("PT")[1]}</h5>
+                            <h5 className="card-text">Departure Date: {items.departure.at.split("T")[0]}</h5>
+                            <h5 className="card-text">Arrival Date: {items.arrival.at.split("T")[0]}</h5>
+                            <h5 className="card-text">Total Amount: {flights.price.total}</h5>
+                        </div>
+                    </div>
+                ))
+            ))}
+        </div>
+    )
+};
+
+export default FlightList
